5dbb3ad400529bf32defa3d64495e2cb3725e509
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-11-05 23:58:46 -0500

    Changed gameobject loading
    Now separates individual gameObjects

--- simulation.py
+++ simulation.py
@@ -1,21 +1,23 @@
 import pygame
 import config
 import utilities
+import math
 
 # ----------------------------------------
 # Initialise the simulation
 # ----------------------------------------
 def init():
     simVars = {
-        # World Points
-        "points": [],
-        "colors": [],
+        # World composition
+        "gameObjects": [],
 
         # Camera variables
-        "cameraCoords": [0, 3, -5],
+        "cameraCoords": [0, 0, -5],
+        "cameraRot": [0, 0, 0],
         "fov": 90,
         "farClip": [100],
         "nearClip": [1],
+        "scale": config.SCALE,
 
         # Loading variables from config.py
         "resolution": config.RESOLUTION,
@@ -44,7 +46,7 @@ def loop(simVars, screen):
         # Display on screen
         handleDisplay(simVars, screen)
         # Limit fps
-        simVars["clock"].tick(60)
+        simVars["clock"].tick(120)
 
 # ----------------------------------------
 # Event and input handling
@@ -74,41 +76,38 @@ def handleEvents(simVars):
         simVars["cameraCoords"][1] += 0.05
     if keys[pygame.K_a]:
         simVars["cameraCoords"][1] -= 0.05
+    if keys[pygame.K_b]:
+        simVars["scale"] += 1
+    if keys[pygame.K_n]:
+        simVars["scale"] -= 1
+    # test buttons
+    if keys[pygame.K_DOWN] or keys[pygame.K_UP]:
+        for i in range(len(simVars["gameObjects"])):
+            object = simVars["gameObjects"][i]
+            for j in range(len(object["points"])):
+                if keys[pygame.K_DOWN]:
+                    object["points"][j] -= 0.05
+                if keys[pygame.K_UP]:
+                    object["points"][j] += 0.05
+
+
+    mouse_move = pygame.mouse.get_rel()
+    simVars["cameraRot"][0] += mouse_move[1]/100
+    simVars["cameraRot"][1] += mouse_move[0]/100
 
 # ----------------------------------------
 # Drawing functions
 # ----------------------------------------
 def handleDisplay(simVars, screen):
 
-    camera = simVars["cameraCoords"]
-    canvasPoints = []
-
     # Clear the screen
-    screen.fill((0,0,0))
+    screen.fill(config.COLOR_BG)
 
-    # Math out the 3d points on the canvas (1 unit away from the camera)
-    for i in range(len(simVars["points"])):
-        point = simVars["points"][i]
-        resolution = simVars["resolution"]
-        ratio = resolution[0]/resolution[1]
+    # Draw the grid
+    #drawGrid(simVars, screen)
 
-
-        # For both axes :
-        # x - cameraX (input camera movement)
-        # / (z - cameraZ) Thales theorem
-        # * ratio (to keep the ratio on resize)
-        # * 300 (to scale up the points) //// 300 is arbitrary
-
-        screen_x = (point[0] - camera[0])/(point[2] - camera[2]) * ratio * 300 + resolution[0]/2
-        screen_y = (point[1] - camera[1])/(-(point[2] - camera[2])) * ratio * 300 + resolution[1]/2
-
-        coords = [screen_x, screen_y]
-        canvasPoints.append(coords)
-    
     # Draw the points
-    for point in canvasPoints:
-        pygame.draw.circle(screen, simVars["color_points"], point, 3)
-
+    drawPoints(simVars, screen)
     # Draw the overlay
     if(not simVars["hide_overlay"]):
         drawOverlay(simVars, screen)
@@ -116,9 +115,44 @@ def handleDisplay(simVars, screen):
     # Apply changes to screen
     pygame.display.flip()
 
+def drawPoints(simVars, screen):
+
+    # Math out the 3d points on the canvas (1 unit away from the camera)
+    for i in range(len(simVars["gameObjects"])):
+        object = simVars["gameObjects"][i]
+        for j in range(len(object["points"])):
+            coords = utilities.vec3tovec2(simVars, object["points"][j])
+            color = utilities.getColor(simVars, object["points"][j])
+
+            # Draw the point
+            pygame.draw.circle(screen, color, coords, 3)
+
+def drawGrid(simVars, screen):
+    x_limits = [-5, 5]
+    z_limits = [-5, 5]
+
+    # Draw the grid
+    for i in range(x_limits[0], x_limits[1] + 1):
+        point_s = [x_limits[0], 0, i]
+        point_e = [x_limits[1], 0, i]
+        coords_s = utilities.vec3tovec2(simVars, point_s)
+        coords_e = utilities.vec3tovec2(simVars, point_e)
+        color = (255, 255, 255)
+        pygame.draw.line(screen, color, coords_s, coords_e, 1)
+    
+    for i in range(z_limits[0], z_limits[1] + 1):
+        point_s = [i, 0, z_limits[0]]
+        point_e = [i, 0, z_limits[1]]
+        coords_s = utilities.vec3tovec2(simVars, point_s)
+        coords_e = utilities.vec3tovec2(simVars, point_e)
+        color = (255, 255, 255)
+        pygame.draw.line(screen, color, coords_s, coords_e, 1)
+
+
+
 def drawOverlay(simVars, screen):
     font = pygame.font.SysFont("Roboto", 30)
-    points = simVars["points"]
+    points = simVars["gameObjects"][0]["points"]
 
     overlay_w = simVars["overlay_size"][0]
     overlay_h = simVars["overlay_size"][1]
