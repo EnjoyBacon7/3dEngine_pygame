aaa0f0e956b098cb97f1c7b57477156759af418d
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-11-06 14:15:43 -0500

    commented

--- args.py
+++ args.py
@@ -6,9 +6,11 @@ def init():
 
     parser.add_argument('-r', '--resolution', nargs=2, type=int, default=(config.RESOLUTION[0], config.RESOLUTION[1]), help='Set the resolution of the window')
     parser.add_argument('-o', '--overlay', action="store_true", default=config.SHOW_OVERLAY, help='Enable the stats overlay')
+    # Rendermode is overwritten to "points" if log is enabled
     parser.add_argument('-rm', '--render-mode', type=str, default=config.RENDER_MODE, help='Set the render mode (wireframe, solid, points)')
     parser.add_argument('-fps', '--fps', type=int, default=config.FPS, help='Set the target FPS')
-    parser.add_argument('-l', '--log', action="store_true", default=False, help='Launch as a logging session (will generate a frame time graph)')
+    parser.add_argument('-l', '--log', action="store_true", default=False, help='Launch as a logging session (will generate a frame time graph). Will forcibly set render mode to "points"')
+
 
     args = parser.parse_args()
 
