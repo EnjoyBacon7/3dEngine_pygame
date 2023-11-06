0e735dc59ff332bd839db1ef017b4131c6640d8f
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-11-06 15:30:25 -0500

    Added product rotation matrix

--- utilities.py
+++ utilities.py
@@ -10,53 +10,51 @@ def updateVarsOnResize(simVars):
 
 def vec3tovec2(simVars, point):
         
-        resolution = simVars["resolution"]
-        ratio = resolution[0]/resolution[1]
-        camera = simVars["cameraCoords"]
-        rotation = simVars["cameraRot"]
+    resolution = simVars["resolution"]
+    ratio = resolution[0]/resolution[1]
+    camera = simVars["cameraCoords"]
+    rotation = simVars["cameraRot"]
 
-        # rotation matrices. Apply rotation to the point (in respect to world origin):
+    # rotation matrices. Apply rotation to the point (in respect to world origin):
 
-        x_rotation = np.array([[1, 0, 0],
-                               [0, math.cos(rotation[0]), -math.sin(rotation[0])],
-                               [0, math.sin(rotation[0]), math.cos(rotation[0])]])
-        y_rotaiton = np.array([[math.cos(rotation[1]), 0, math.sin(rotation[1])],
-                               [0, 1, 0],
-                               [-math.sin(rotation[1]), 0, math.cos(rotation[1])]])
-        z_rotation = np.array([[math.cos(rotation[2]), -math.sin(rotation[2]), 0],
-                               [math.sin(rotation[2]), math.cos(rotation[2]), 0],
-                               [0, 0, 1]])
+    cos_y = math.cos(rotation[1])
+    sin_y = math.sin(rotation[1])
+    cos_z = math.cos(rotation[2])
+    sin_z = math.sin(rotation[2])
+    cos_x = math.cos(rotation[0])
+    sin_x = math.sin(rotation[0])
 
+    rotation_matrix = np.array([
+            [cos_z * cos_y, cos_z * sin_y * sin_x - sin_z * cos_x, cos_z * sin_y * cos_x + sin_z * sin_x],
+            [sin_z * cos_y, sin_z * sin_y * sin_x + cos_z * cos_x, sin_z * sin_y * cos_x - cos_z * sin_x],
+            [-sin_y, cos_y * sin_x, cos_y * cos_x],
+            ])
+    point = np.array(point)
+    point = np.matmul(rotation_matrix, point)
 
-        # Apply rotation matrices to the point
-        point = np.array(point)
-        point = np.matmul(x_rotation, point)
-        point = np.matmul(y_rotaiton, point)
-        point = np.matmul(z_rotation, point)
+    # For both axes :
+    # x - cameraX (input camera movement)
+    # / (z - cameraZ) Thales theorem
+    # * ratio (to keep the ratio on resize)
+    # * 300 (to scale up the points) //// 300 is arbitrary
 
-        # For both axes :
-        # x - cameraX (input camera movement)
-        # / (z - cameraZ) Thales theorem
-        # * ratio (to keep the ratio on resize)
-        # * 300 (to scale up the points) //// 300 is arbitrary
-
-        if point[2] - camera[2] == 0:
-            relative_x = 0
-            relative_y = 0
-        elif(point[2] - camera[2] < 0):
-            screen_x = -1
-            screen_y = -1
-            return (screen_x, screen_y)
-        else:
-            relative_x = (point[0] - camera[0])/(point[2] - camera[2])
-            relative_y = (point[1] - camera[1])/(-(point[2] - camera[2]))
-
-        screen_x = relative_x * ratio * simVars["scale"] + resolution[0]/2
-        screen_y = relative_y * ratio * simVars["scale"] + resolution[1]/2
-
-        simVars["log"][simVars["render_mode"]]["rendered_points"] += 1
-
+    if point[2] - camera[2] == 0:
+        relative_x = 0
+        relative_y = 0
+    elif(point[2] - camera[2] < 0):
+        screen_x = -1
+        screen_y = -1
         return (screen_x, screen_y)
+    else:
+        relative_x = (point[0] - camera[0])/(point[2] - camera[2])
+        relative_y = (point[1] - camera[1])/(-(point[2] - camera[2]))
+
+    screen_x = relative_x * ratio * simVars["scale"] + resolution[0]/2
+    screen_y = relative_y * ratio * simVars["scale"] + resolution[1]/2
+
+    simVars["log"][simVars["render_mode"]]["rendered_points"] += 1
+
+    return (screen_x, screen_y)
 
 def getColor(simVars, point):   
     dst_to_origin = math.sqrt(point[0] * point[0] + point[1] * point[1] + point[2] * point[2])
