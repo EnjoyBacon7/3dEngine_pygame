da4800b54d8b0194fe82f85425c207784f5aaa66
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-11-06 01:11:14 -0500

    added solid and wireframe views

--- simulation.py
+++ simulation.py
@@ -6,6 +6,8 @@ import math
 # ----------------------------------------
 # Initialise the simulation
 # ----------------------------------------
+
+
 def init():
     simVars = {
         # World composition
@@ -29,13 +31,24 @@ def init():
         "color_points": config.COLOR_POINTS,
         "hide_overlay": config.HIDE_OVERLAY,
 
+        # Rendering variables
+        "render_mode": "solid",  # "wireframe", "solid", or "points"
+
         "clock": pygame.time.Clock(),
+
+        # Logging variables
+        "log": {
+            "rendered_points": 0,
+            "rendered_faces": 0,
+        }
     }
     return simVars
 
 # ----------------------------------------
 # Main loop
 # ----------------------------------------
+
+
 def loop(simVars, screen):
 
     running = True
@@ -51,17 +64,19 @@ def loop(simVars, screen):
 # ----------------------------------------
 # Event and input handling
 # ----------------------------------------
+
+
 def handleEvents(simVars):
     for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.VIDEORESIZE:
+            utilities.updateVarsOnResize(simVars)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
                 pygame.quit()
-            if event.type == pygame.VIDEORESIZE:
-                utilities.updateVarsOnResize(simVars)
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                if event.key == pygame.K_SPACE:
-                    simVars["hide_overlay"] = not simVars["hide_overlay"]
+            if event.key == pygame.K_SPACE:
+                simVars["hide_overlay"] = not simVars["hide_overlay"]
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_s]:
@@ -90,7 +105,6 @@ def handleEvents(simVars):
                 if keys[pygame.K_UP]:
                     object["points"][j] += 0.05
 
-
     mouse_move = pygame.mouse.get_rel()
     simVars["cameraRot"][0] += mouse_move[1]/100
     simVars["cameraRot"][1] += mouse_move[0]/100
@@ -98,34 +112,76 @@ def handleEvents(simVars):
 # ----------------------------------------
 # Drawing functions
 # ----------------------------------------
+
+
 def handleDisplay(simVars, screen):
 
     # Clear the screen
     screen.fill(config.COLOR_BG)
 
     # Draw the grid
-    #drawGrid(simVars, screen)
+    # drawGrid(simVars, screen)
 
     # Draw the points
-    drawPoints(simVars, screen)
+    drawWorld(simVars, screen)
     # Draw the overlay
-    if(not simVars["hide_overlay"]):
+    if (not simVars["hide_overlay"]):
         drawOverlay(simVars, screen)
 
     # Apply changes to screen
     pygame.display.flip()
 
-def drawPoints(simVars, screen):
+
+def drawWorld(simVars, screen):
+
 
     # Math out the 3d points on the canvas (1 unit away from the camera)
-    for i in range(len(simVars["gameObjects"])):
-        object = simVars["gameObjects"][i]
-        for j in range(len(object["points"])):
-            coords = utilities.vec3tovec2(simVars, object["points"][j])
-            color = utilities.getColor(simVars, object["points"][j])
+    if (simVars["render_mode"] == "wireframe"):
+        for object in simVars["gameObjects"]:
+            for face in object["faces"]:
+                face_2D = [
+                    utilities.vec3tovec2(simVars, object["points"][face[0] - 1]),
+                    utilities.vec3tovec2(simVars, object["points"][face[1] - 1]),
+                    utilities.vec3tovec2(simVars, object["points"][face[2] - 1]),
+                ]
+                colors = [
+                    utilities.getColor(simVars, object["points"][face[0] - 1]),
+                    utilities.getColor(simVars, object["points"][face[1] - 1]),
+                    utilities.getColor(simVars, object["points"][face[2] - 1]),
+                ]
 
-            # Draw the point
-            pygame.draw.circle(screen, color, coords, 3)
+                simVars["log"]["rendered_points"] += 3
+                pygame.draw.line(screen, colors[0], face_2D[0], face_2D[1])
+                pygame.draw.line(screen, colors[1], face_2D[1], face_2D[2])
+                pygame.draw.line(screen, colors[2], face_2D[2], face_2D[0])
+
+    elif (simVars["render_mode"] == "solid"):
+        for i in range(len(simVars["gameObjects"])):
+            object = simVars["gameObjects"][i]
+            for j in range(len(object["faces"])):
+                face = object["faces"][j]
+                face_2D = [
+                    utilities.vec3tovec2(simVars, object["points"][face[0] - 1]),
+                    utilities.vec3tovec2(simVars, object["points"][face[1] - 1]),
+                    utilities.vec3tovec2(simVars, object["points"][face[2] - 1]),
+                ]
+                color = utilities.getColor(simVars, object["points"][face[0] - 1])
+
+                simVars["log"]["rendered_points"] += 3
+                simVars["log"]["rendered_faces"] += 1
+                pygame.draw.polygon(screen, color, face_2D)
+
+    elif (simVars["render_mode"] == "points"):
+        for i in range(len(simVars["gameObjects"])):
+            object = simVars["gameObjects"][i]
+            for j in range(len(object["points"])):
+                point_2D = utilities.vec3tovec2(simVars, object["points"][j])
+                color = utilities.getColor(simVars, object["points"][j])
+
+                # Draw the point
+                simVars["log"]["rendered_points"] += 1
+                pygame.draw.circle(screen, color, point_2D, 3)
+
 
 def drawGrid(simVars, screen):
     x_limits = [-5, 5]
@@ -139,7 +195,7 @@ def drawGrid(simVars, screen):
         coords_e = utilities.vec3tovec2(simVars, point_e)
         color = (255, 255, 255)
         pygame.draw.line(screen, color, coords_s, coords_e, 1)
-    
+
     for i in range(z_limits[0], z_limits[1] + 1):
         point_s = [i, 0, z_limits[0]]
         point_e = [i, 0, z_limits[1]]
