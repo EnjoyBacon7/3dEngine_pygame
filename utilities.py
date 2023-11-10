3a0e4a5db82e69938a0204e23f9a3b866afbb472
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-11-10 13:59:34 -0500

    Added better var calculations on resize

--- utilities.py
+++ utilities.py
@@ -6,8 +6,13 @@ import math
 
 def updateVarsOnResize(simVars):
     simVars["resolution"] = pygame.display.get_surface().get_size()
-    simVars["overlay_size"] = (config.OVERLAY_SIZE[0]/100 * simVars["resolution"][0], config.OVERLAY_SIZE[1]/100 * simVars["resolution"][1])
-    simVars["overlay_pos"] = (config.OVERLAY_POS[0]/100 * simVars["resolution"][0], config.OVERLAY_POS[1]/100 * simVars["resolution"][1])
+    simVars["overlay_size"] = (config.OVERLAY_SIZE[0]/100 * simVars["resolution"]
+                               [0], config.OVERLAY_SIZE[1]/100 * simVars["resolution"][1])
+    simVars["overlay_pos"] = (config.OVERLAY_POS[0]/100 * simVars["resolution"]
+                              [0], config.OVERLAY_POS[1]/100 * simVars["resolution"][1])
+    simVars["projection_matrix"] = getProjectionMatrix(simVars)
+
+
 def getProjectionMatrix(simVars):
     # Create projection matrix
     nearClip = simVars["nearClip"]
