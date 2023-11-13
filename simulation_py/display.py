bbbf65812c844cb527f1c84b1dcaf84c0365661d
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-11-12 23:30:37 -0500

    Added point pre calculating to solid view
    Removed face normal calculations (cross product too computationally expensive)

--- simulation_py/display.py
+++ simulation_py/display.py
@@ -132,29 +132,40 @@ def drawWorld(simVars, screen):
             (255, 0, 127)
         ]
         for object in simVars["gameObjects"]:
+
+            pre_baked_points = []
+            pre_baked_colors = []
+
+            for point in object["points"]:
+                pre_baked_points.append(utilities.vec3tovec2(simVars, point, camera_rotation_matrix))
+                #pre_baked_colors.append(utilities.getColor(simVars, point))
             
             for i, face in enumerate(object["faces"]):
-                normal = utilities.getFaceNormal(simVars, [
-                    object["points"][face[0] - 1],
-                    object["points"][face[1] - 1],
-                    object["points"][face[2] - 1]
-                ])
-                if (object["points"][face[0] - 1][0] - simVars["cameraCoords"][0]) * normal[0] + (object["points"][face[0] - 1][1] - simVars["cameraCoords"][1]) * normal[1] + (object["points"][face[0] - 1][2] - simVars["cameraCoords"][2]) * normal[2] < 0:                                        
-                    
-                    face_2D = [
-                        utilities.vec3tovec2(simVars, object["points"][face[0] - 1], camera_rotation_matrix),
-                        utilities.vec3tovec2(simVars, object["points"][face[1] - 1], camera_rotation_matrix),
-                        utilities.vec3tovec2(simVars, object["points"][face[2] - 1], camera_rotation_matrix)
-                    ]
+                
+                # Project the face's points
+                face_2D = [
+                    pre_baked_points[face[0] - 1],
+                    pre_baked_points[face[1] - 1],
+                    pre_baked_points[face[2] - 1],
+                ]
+                color = colors[i%12]
 
-                    if face_2D[0] == (-1, -1) or face_2D[1] == (-1, -1) or face_2D[2] == (-1, -1):
-                        continue
+                # If one of the points is behind the camera, don't render the face
+                if face_2D[0] == (-1, -1) or face_2D[1] == (-1, -1) or face_2D[2] == (-1, -1):
+                    continue
 
-                    #color = utilities.getColor(simVars, object["points"][face[0] - 1])
-                    color = colors[i%12]
-                    # Color gradient not implemented
-                    simVars["log"]["solid"]["rendered_faces"] += 1
-                    pygame.draw.polygon(screen, color, face_2D)
+                # # If the winding order is CCW, don't render the face
+                # abX = face_2D[1][0] - face_2D[0][0]
+                # abY = face_2D[1][1] - face_2D[0][1]
+                # acX = face_2D[2][0] - face_2D[0][0]
+                # acY = face_2D[2][1] - face_2D[0][1]
+
+                # if (abX * acY - abY * acX) < 0:
+                #     continue
+
+                # Color gradient not implemented
+                simVars["log"]["solid"]["rendered_faces"] += 1
+                pygame.draw.polygon(screen, color, face_2D)
 
 def drawGrid(simVars, screen):
     x_limits = [-5, 5]
