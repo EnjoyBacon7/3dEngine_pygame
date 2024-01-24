f5c41728983827a94fe8ad5e75a7281bb99ad94d
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2024-01-24 18:06:51 +0100

    reformat

--- args.py
+++ args.py
@@ -16,15 +16,9 @@ def init():
 
     parser = argparse.ArgumentParser(description='3D engine in python')
 
-    parser.add_argument('-r', '--resolution', nargs=2, type=int,
-                        default=(defaults["resolution"][0],
-                                 defaults["resolution"][1]),
-                        help='Set the resolution of the window')
-    parser.add_argument('-o', '--overlay', action="store_true",
-                        default=defaults["show_overlay"], help='Enable the stats overlay')
-    parser.add_argument(
-        '-rm', '--render-mode', type=str, default=defaults["render_mode"],
-        help='Set the render mode (wireframe, solid, points)')
+    parser.add_argument('-r', '--resolution', nargs=2, type=int, default=(defaults["resolution"][0], defaults["resolution"][1]), help='Set the resolution of the window')
+    parser.add_argument('-o', '--overlay', action="store_true", default=defaults["show_overlay"], help='Enable the stats overlay')
+    parser.add_argument('-rm', '--render-mode', type=str, default=defaults["render_mode"], help='Set the render mode (wireframe, solid, points)')
     parser.add_argument('-fps', '--fps', type=int, default=defaults["fps"], help='Set the target FPS')
 
     args = parser.parse_args()
